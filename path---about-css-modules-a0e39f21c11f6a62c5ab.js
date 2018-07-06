9c2f14c02c0f7a24614b7943ee7c9824195fc5ec
CORP\daniela.desira <daniela.desira@igamingcloud.com>
2018-07-06 16:18:14 +0200

    Updates

--- path---about-css-modules-a0e39f21c11f6a62c5ab.js
+++ path---about-css-modules-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x788aad75d9fd],{301:function(t,a){t.exports={pathContext:{}}}});
+webpackJsonp([0x788aad75d9fd],{312:function(t,a){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---about-css-modules-a0e39f21c11f6a62c5ab.js.map
---
+++ path---index-6fbaef4d54901f368cfc.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{314:function(t,e){t.exports={data:{site:{siteMetadata:{title:"my personal portfolio"}}},pathContext:{}}}});
+//# sourceMappingURL=path---index-6fbaef4d54901f368cfc.js.map
