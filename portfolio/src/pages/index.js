799b6e4c67e9e71d7afa70c6a3d4e53509686627
CORP\daniela.desira <daniela.desira@igamingcloud.com>
2018-07-06 16:15:17 +0200

    starting with grapgql

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -2,12 +2,24 @@
 
 import React from "react"
 
-export default () => (
+export default ({ data }) => (
   <div style={{ margin: "3rem auto", maxWidth: 600 }}>
     <h1>Great Stuff Coming Soon!!!</h1>
     <p>
+      You have stumbled across {data.site.siteMetadata.title}
+      <br />
       What do I like to do? Lots of course but definitely enjoy building
       websites.
     </p>
   </div>
 );
+
+export const query = graphql`
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
